ebe951761eeb0ff511ed78435c272b91fb1393f5
João Pedro Caetano <caetanojpo@gmail.com>
2023-02-03 15:20:01 -0300

    ajustes

--- src/components/formCoworking/index.jsx
+++ src/components/formCoworking/index.jsx
@@ -1,3 +1,4 @@
+import { Box, Flex, Heading, Stack, Text } from "@chakra-ui/react";
 import Button from "../button";
 
 
@@ -5,6 +6,14 @@ function FormCoworking() {
 
     return (
         <>
+        <Flex>
+            <Box>
+                <Stack>
+                    <Heading>FORMULÁRIO COWORKING</Heading>
+                    <Text> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quasi aliquam velit adipisci quam, sit reprehenderit voluptate. Id itaque in, at praesentium reprehenderit, soluta, voluptatum provident perferendis quos nobis earum!</Text>
+                </Stack>
+            </Box>
+        </Flex>
         </>
     )
 }
