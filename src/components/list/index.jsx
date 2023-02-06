a2599c7d6ba26914b96a521e0dd5adbc6af6bc0b
João Pedro Caetano <caetanojpo@gmail.com>
2023-02-06 19:58:20 -0300

    tooltip nos botões

--- src/components/list/index.jsx
+++ src/components/list/index.jsx
@@ -1,54 +1,54 @@
-import { Button, Flex, ListItem, Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr, UnorderedList } from "@chakra-ui/react";
+import { Button, Flex, ListItem, Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tooltip, Tr, UnorderedList } from "@chakra-ui/react";
 import { Icon } from "@iconify/react";
 import TdForms from "./td/tdForms";
 
 export default function ListForms() {
-    return (
-        <>
-        <Flex padding='32px' fontFamily='gotham' flexDir='column' w='full' h='100vh' bg='#003b33' justifyContent='space-around' color='white' boxShadow='6px 6px 15px #c0cad4,
+  return (
+    <>
+      <Flex padding='32px' fontFamily='gotham' flexDir='column' w='full' h='100vh' bg='#003b33' justifyContent='center' color='white' gap='10px' boxShadow='6px 6px 15px #c0cad4,
              -6px -6px 15px #f4ffff;'>
-
-<TableContainer borderRadius='8px' bg='white'>
-  <Table variant='striped' colorScheme='green' color='black'>
-    <TableCaption>Formulários das Reuniões</TableCaption>
-    <Thead > 
-      <Tr>
-        <Th>Formulários</Th>
-        
-
-        <Th>
-       
-            <Button>
-            <Icon icon="material-symbols:new-window" />
-            </Button>
+        <Flex justifyContent='flex-end'>
+        <Tooltip hasArrow label='Adicione um novo formulário' bg='gray.300' color='black'>
+        <Button w='60px'bg='white'>
+          <Icon color='black' icon="material-symbols:new-window" width="30" height="30" />
+          </Button>
+</Tooltip>
          
-        </Th>
-        
-       
-      </Tr>
-    </Thead>
-    <Tbody >
-      <TdForms
-      rotaForms=''
-      formsTitulo="Formulário Reunião 01/01/2001    "
-      />
-      <TdForms
-      rotaForms=''
-      formsTitulo="Formulário Reunião 01/01/2001    "
-      />
-      <TdForms
-      rotaForms=''
-      formsTitulo="Formulário Reunião 01/01/2001    "
-      />
-      
-    </Tbody>
-    
-  </Table>
-</TableContainer>
-                
-
         </Flex>
-            
-        </>
-    )
+
+        <TableContainer borderRadius='8px' bg='white'>
+          <Table variant='striped' colorScheme='green' color='black'>
+            <TableCaption>Formulários das Reuniões</TableCaption>
+            <Thead >
+              <Tr>
+                <Th>Formulários</Th>
+
+
+
+              </Tr>
+            </Thead>
+            <Tbody >
+              <TdForms
+                rotaForms=''
+                formsTitulo="Formulário Reunião 01/01/2001    "
+              />
+              <TdForms
+                rotaForms=''
+                formsTitulo="Formulário Reunião 01/01/2001    "
+              />
+              <TdForms
+                rotaForms=''
+                formsTitulo="Formulário Reunião 01/01/2001    "
+              />
+
+            </Tbody>
+
+          </Table>
+        </TableContainer>
+
+
+      </Flex>
+
+    </>
+  )
 }
--- src/components/list/td/tdForms/index.jsx
+++ src/components/list/td/tdForms/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Link, Stack, Td, Tr } from "@chakra-ui/react";
+import { Box, Button, Flex, Link, Stack, Td, Tooltip, Tr } from "@chakra-ui/react";
 import { Icon } from "@iconify/react";
 
 export default function TdForms(props) {
@@ -17,13 +17,21 @@ export default function TdForms(props) {
         
         <Td >
             <Flex justifyContent='flex-end'>
-
+            
+            <Tooltip hasArrow label='Ver detalhes' bg='gray.300' color='black'>
             <Button  mr='10px'>
             <Icon icon="ic:outline-remove-red-eye" />
             </Button>
+            </Tooltip>
+           
+            <Tooltip hasArrow label='Editar formulário' bg='gray.300' color='black'>
             <Button mr='10px'> <Icon icon="material-symbols:edit" /></Button>
+            </Tooltip>
+            <Tooltip hasArrow label='Excluir formulário' bg='gray.300' color='black'>
             <Button><Icon icon="material-symbols:delete-outline"/></Button>
+            </Tooltip>
             </Flex>
+            
           
         </Td>
       </Tr>
