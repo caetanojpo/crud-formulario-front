0e9a254ab669cbbfa962e776c4eb50bf8a5ce43f
João Pedro Caetano <caetanojpo@gmail.com>
2023-02-06 22:18:06 -0300

    alert no botão deletar formulario

--- src/components/list/td/tdForms/index.jsx
+++ src/components/list/td/tdForms/index.jsx
@@ -1,7 +1,10 @@
-import { Box, Button, Flex, Link, Stack, Td, Tooltip, Tr } from "@chakra-ui/react";
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Box, Button, Flex, Link, Stack, Td, Tooltip, Tr, useDisclosure } from "@chakra-ui/react";
 import { Icon } from "@iconify/react";
+import { useRef } from "react";
 
 export default function TdForms(props) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = useRef()
     return(
         <>
         <Tr>
@@ -28,13 +31,41 @@ export default function TdForms(props) {
             <Button mr='10px'> <Icon icon="material-symbols:edit" /></Button>
             </Tooltip>
             <Tooltip hasArrow label='Excluir formulário' bg='gray.300' color='black'>
-            <Button><Icon icon="material-symbols:delete-outline"/></Button>
+            <Button onClick={onOpen}><Icon icon="material-symbols:delete-outline"/></Button>
             </Tooltip>
+           
             </Flex>
             
           
         </Td>
       </Tr>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              Deletar Formulário
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Tem certeza que deseja deletar o formulário?
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancelar
+              </Button>
+              <Button colorScheme='red' onClick={onClose} ml={3}>
+                Deletar
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
         </>
     )
 }
