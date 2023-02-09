1b413a91eab353d707079090689e6a016d8c17a4
João Pedro Caetano <caetanojpo@gmail.com>
2023-02-09 18:55:07 -0300

    Navbar updated

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,10 +1,23 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 import Dropdown from './dropdown';
 
 export function Navbar() {
   return (
-    <Flex justifyContent="space-evenly" height="50px;" bg="#00A99D">
-      <Flex>Fomenta</Flex>
+    <Flex justifyContent="space-evenly" height="70px;" bg="#00A99D">
+      <Flex alignItems="center" justifyContent="center" className="logoNavbar">
+        <Link to="/">
+          <Text
+            fontSize="24px"
+            fontFamily="gotham"
+            fontWeight="700"
+            color="white"
+          >
+            FOMENTA VALE
+          </Text>
+        </Link>
+      </Flex>
+
       <Dropdown
         title="Reunião"
         listPath="/reuniao"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,10 @@
+import { Flex } from '@chakra-ui/react';
+import './style.css';
+
 export default function Home() {
-  return <>Home</>;
+  return (
+    <>
+      <Flex className="homepage" h="100vh" w="100%" />
+    </>
+  );
 }
